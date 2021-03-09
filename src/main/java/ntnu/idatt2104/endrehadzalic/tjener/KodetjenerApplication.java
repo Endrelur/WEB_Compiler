efa77684b2366eafed1759a6f31c8b9775cacd48
Endré Hadzalic <endre.had@gmail.com>
2021-03-09 13:39:10 +0100

    Initial commit

---
+++ src/main/java/ntnu/idatt2104/endrehadzalic/tjener/KodetjenerApplication.java
@@ -0,0 +1,13 @@
+package ntnu.idatt2104.endrehadzalic.tjener;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KodetjenerApplication {
+
+	public static void main(String[] args)
+		SpringApplication.run(KodetjenerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ntnu/idatt2104/endrehadzalic/tjener/KodetjenerApplicationTests.java
@@ -0,0 +1,13 @@
+package ntnu.idatt2104.endrehadzalic.tjener;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KodetjenerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
